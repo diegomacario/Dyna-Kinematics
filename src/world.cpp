0add5c741257f8b5af94a8e2b49b58042c8ecc9d
diegomacario <diego.macario5@gmail.com>
2020-04-01 08:34:09 -0400

    Implement the classical 4th order Runge-Kutta method

--- src/world.cpp
+++ src/world.cpp
@@ -88,6 +88,7 @@ void World::computeForces()
    }
 }
 
+/*
 void World::integrate(float deltaTime)
 {
    for (std::vector<RigidBody2D>::iterator iter = mRigidBodies.begin(); iter != mRigidBodies.end(); ++iter)
@@ -95,21 +96,117 @@ void World::integrate(float deltaTime)
       RigidBody2D::KinematicAndDynamicState& currentState = iter->mStates[0];
       RigidBody2D::KinematicAndDynamicState& futureState  = iter->mStates[1];
 
-      // Calculate new position and orientation
-      futureState.positionOfCenterOfMass = currentState.positionOfCenterOfMass + (currentState.velocityOfCenterOfMass * deltaTime);
-      futureState.orientation = currentState.orientation + (currentState.angularVelocity * deltaTime);
+      // Calculate new position and velocity using Euler's method
 
-      // Calculate new velocity and angular velocity
+      // X_n+1 = X_n + (h * V_n)
+      futureState.positionOfCenterOfMass = currentState.positionOfCenterOfMass + (currentState.velocityOfCenterOfMass * deltaTime);
 
       // F = (d/dt)P = M * A
       // A = F / M
       // V_n+1 = V_n + (h * A) = V_n + (h * (F / M))
       futureState.velocityOfCenterOfMass = currentState.velocityOfCenterOfMass + ((deltaTime * iter->mOneOverMass) * currentState.forceOfCenterOfMass);
 
+      // Calculate new orientation and angular velocity using Euler's method
+
+      // O_n+1 = O_n + (h * W_n)
+      futureState.orientation = currentState.orientation + (currentState.angularVelocity * deltaTime);
+
       // T = (d/dt)L = I * Alpha
       // Alpha = T / I
       // W_n+1 = W_n + (h * Alpha) = W_n + (h * (T / I))
       futureState.angularVelocity = currentState.angularVelocity + ((deltaTime * iter->mOneOverMomentOfInertia) * currentState.torque);
+   }
+}
+*/
+
+void World::integrate(float deltaTime)
+{
+   for (std::vector<RigidBody2D>::iterator iter = mRigidBodies.begin(); iter != mRigidBodies.end(); ++iter)
+   {
+      RigidBody2D::KinematicAndDynamicState& currentState = iter->mStates[0];
+      RigidBody2D::KinematicAndDynamicState& futureState  = iter->mStates[1];
+
+      float midPointOfDeltaTime = deltaTime / 2.0f;
+
+      // Calculate new position and velocity using the classical 4th order Runge-Kutta method
+
+      // We want to solve this 2nd order ODE:
+      // F = M * X^dotdot
+      // We can solve it by writing it as a system of two 1rst order ODEs:
+      // [X^dot] = [  V  ]
+      // [V^dot]   [F / M]
+      // Where we want to find the position (X) and the velocity (V)
+
+      glm::vec2 k1Pos                     = currentState.velocityOfCenterOfMass;
+      glm::vec2 k1Vel                     = (iter->mOneOverMass * currentState.forceOfCenterOfMass);
+
+      glm::vec2 posAtMidPointOfDeltaTime1 = currentState.positionOfCenterOfMass + (k1Pos * midPointOfDeltaTime);
+      glm::vec2 velAtMidPointOfDeltaTime1 = currentState.velocityOfCenterOfMass + (k1Vel * midPointOfDeltaTime);
+
+      glm::vec2 k2Pos                     = velAtMidPointOfDeltaTime1;
+      glm::vec2 k2Vel                     = (iter->mOneOverMass * currentState.forceOfCenterOfMass);
+
+      glm::vec2 posAtMidPointOfDeltaTime2 = currentState.positionOfCenterOfMass + (k2Pos * midPointOfDeltaTime);
+      glm::vec2 velAtMidPointOfDeltaTime2 = currentState.velocityOfCenterOfMass + (k2Vel * midPointOfDeltaTime);
+
+      glm::vec2 k3Pos                     = velAtMidPointOfDeltaTime2;
+      glm::vec2 k3Vel                     = (iter->mOneOverMass * currentState.forceOfCenterOfMass);
+
+      glm::vec2 posAtDeltaTime            = currentState.positionOfCenterOfMass + (k3Pos * deltaTime);
+      glm::vec2 velAtDeltaTime            = currentState.velocityOfCenterOfMass + (k3Vel * deltaTime);
+
+      glm::vec2 k4Pos                     = velAtDeltaTime;
+      glm::vec2 k4Vel                     = (iter->mOneOverMass * currentState.forceOfCenterOfMass);
+
+      glm::vec2 weightedAverageOfPositionSlopes = ((k1Pos + (2.0f * k2Pos) + (2.0f * k3Pos) + k4Pos) / 6.0f);
+      glm::vec2 weightedAverageOfVelocitySlopes = ((k1Vel + (2.0f * k2Vel) + (2.0f * k3Vel) + k4Vel) / 6.0f);
+
+      // P_n+1 = P_n + (h * weightedAverageOfPositionSlopes)
+      futureState.positionOfCenterOfMass = currentState.positionOfCenterOfMass + (weightedAverageOfPositionSlopes * deltaTime);
+
+      // V_n+1 = V_n + (h * weightedAverageOfVelocitySlopes)
+      futureState.velocityOfCenterOfMass = currentState.velocityOfCenterOfMass + (weightedAverageOfVelocitySlopes * deltaTime);
+
+      // Calculate new orientation and angular velocity using the classical 4th order Runge-Kutta method
+
+      // We want to solve this 2nd order ODE:
+      // T = I * O^dotdot
+      // We can solve it by writing it as a system of two 1rst order ODEs:
+      // [O^dot] = [  W  ]
+      // [W^dot]   [T / I]
+      // Where we want to find the orientation (O) and the angular velocity (W)
+
+      float k1Ori                     = currentState.angularVelocity;
+      float k1Ang                     = (iter->mOneOverMomentOfInertia * currentState.torque);
+
+      float oriAtMidPointOfDeltaTime1 = currentState.orientation + (k1Ori * midPointOfDeltaTime);
+      float angAtMidPointOfDeltaTime1 = currentState.angularVelocity + (k1Ang * midPointOfDeltaTime);
+
+      float k2Ori                     = angAtMidPointOfDeltaTime1;
+      float k2Ang                     = (iter->mOneOverMomentOfInertia * currentState.torque);
+
+      float oriAtMidPointOfDeltaTime2 = currentState.orientation + (k2Ori * midPointOfDeltaTime);
+      float angAtMidPointOfDeltaTime2 = currentState.angularVelocity + (k2Ang * midPointOfDeltaTime);
+
+      float k3Ori                     = angAtMidPointOfDeltaTime2;
+      float k3Ang                     = (iter->mOneOverMomentOfInertia * currentState.torque);
+
+      float oriAtDeltaTime            = currentState.orientation + (k3Ori * deltaTime);
+      float angAtDeltaTime            = currentState.angularVelocity + (k3Ang * deltaTime);
+
+      float k4Ori                     = angAtDeltaTime;
+      float k4Ang                     = (iter->mOneOverMomentOfInertia * currentState.torque);
+
+      float weightedAverageOfOrientationSlopes = ((k1Ori + (2.0f * k2Ori) + (2.0f * k3Ori) + k4Ori) / 6.0f);
+      float weightedAverageOfAngularVelocitySlopes = ((k1Ang + (2.0f * k2Ang) + (2.0f * k3Ang) + k4Ang) / 6.0f);
+
+      // O_n+1 = O_n + (h * W_n)
+      futureState.orientation = currentState.orientation + (weightedAverageOfOrientationSlopes * deltaTime);
+
+      // T = (d/dt)L = I * Alpha
+      // Alpha = T / I
+      // W_n+1 = W_n + (h * Alpha) = W_n + (h * (T / I))
+      futureState.angularVelocity = currentState.angularVelocity + (weightedAverageOfAngularVelocitySlopes * deltaTime);
    }
 }
 
